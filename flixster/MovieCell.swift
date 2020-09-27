1ebd762971bcafae554b8ed7bfdde57bd15c712b
Nesha Prabahar <nprabahar3@gatech.edu>
2020-09-27 15:39:39 -0400

    Podfile added

---
+++ flixster/MovieCell.swift
@@ -0,0 +1,24 @@
+//
+//  MovieCell.swift
+//  flixster
+//
+//  Created by Nesha Prabahar on 27/09/20.
+//  Copyright © 2020 neshaprabahar. All rights reserved.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
---
+++ flixster/MoviesViewController.swift
@@ -0,0 +1,30 @@
+//
+//  UserViewController.swift
+//  flixster
+//
+//  Created by Nesha Prabahar on 27/09/20.
+//  Copyright © 2020 neshaprabahar. All rights reserved.
+//
+
+import UIKit
+
+class UserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
